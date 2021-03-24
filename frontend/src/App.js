f4bf9deac35ef4cd5ca6e15b3c50d02f97496706
dastan <dastansulaimankulov98@gmail.com>
2021-03-24 17:44:03 +0600

    changes-2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import Modal from "./Components/UI/Modal/Modal";
-import BoardForm from "./Components/BoardForm/BoardForm";
-import Card from "./Components/Card/Card";
 
 const App = () => {
 
 	return (
 		<div>
-			<Modal show={false}>
-				<BoardForm/>
-			</Modal>
-			<Card/>
+			App
 		</div>
 	);
 };
