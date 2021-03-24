18148e216fe3d9ad6a44788e2618f84730620b05
dastan <dastansulaimankulov98@gmail.com>
2021-03-24 17:21:23 +0600

    on case

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://dastan:trello1234@cluster0.thipr.mongodb.net/boards?retryWrites=true&w=majority',
+    ),
+    AuthModule,
+  ],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
