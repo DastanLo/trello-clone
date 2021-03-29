ac0eb3bae575069b0c210dc446cf14a52f63001e
Colorados <fall3nleaf@gmail.com>
2021-03-29 16:33:30 +0600

    finished

---
+++ frontend/src/container/BoardsPage.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import CreateBoard from "../components/CreateBoard/CreateBoard";
+import Board from "../components/Board/Board";
+
+const BoardsPage = () => {
+    return (
+        <div className="boards-container">
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <Board/>
+            <CreateBoard/>
+        </div>
+    );
+};
+
+export default BoardsPage;
