30446a044019ba958d1f84cc412deda67067cb8d
dastan <dastansulaimankulov98@gmail.com>
2021-03-30 18:36:03 +0600

    style fixes

--- frontend/src/store/reducers/boardReducer.js
+++ frontend/src/store/reducers/boardReducer.js
@@ -10,7 +10,7 @@ const boardReducer = (state = initialState, action) => {
   switch (action.type) {
     case ac.GET_BOARD_START:
       return {...state, loading: true};
-    case ac.CREATE_BOARD_SUCCESS:
+    case ac.GET_BOARD_SUCCESS:
       return {...state, loading: false, boards: action.payload};
     case ac.GET_BOARD_ERROR:
       return {...state, loading: false, error: action.payload};
