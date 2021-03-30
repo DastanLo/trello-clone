425dcfcf1f8bea9ff53e13f1b5f668d03b5b2e98
dastan <dastansulaimankulov98@gmail.com>
2021-03-30 16:36:06 +0600

    finished merge

--- frontend/src/components/BoardForm/BoardForm.jsx
+++ frontend/src/components/BoardForm/BoardForm.jsx
@@ -14,7 +14,6 @@ const BoardForm = () => {
 	const inputChangeHandler = (e) => {
 		const titleValue = e.target.value;
 		setTitle(titleValue);
-		console.log(title)
 	}
 
 	const createDesk = () => {
