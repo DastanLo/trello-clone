75f59eb7de15c6e69e3450231d8d1166295bd24e
Colorados <fall3nleaf@gmail.com>
2021-03-30 16:31:38 +0600

    finished modal call

--- frontend/src/container/BoardsPage.jsx
+++ frontend/src/container/BoardsPage.jsx
@@ -1,32 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CreateBoard from "../components/CreateBoard/CreateBoard";
 import Board from "../components/Board/Board";
+import Modal from "../components/UI/Modal/Modal";
+import BoardForm from "../components/BoardForm/BoardForm";
 
 const BoardsPage = () => {
+    const [openModal, setOpenModal] = useState(false);
+    const toggle = () => {
+        setOpenModal(!openModal);
+    }
     return (
         <div className="boards-container">
+            <Modal show={openModal}>
+                <BoardForm/>
+            </Modal>
             <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <Board/>
-            <CreateBoard/>
+            <CreateBoard click={toggle}/>
         </div>
     );
 };
