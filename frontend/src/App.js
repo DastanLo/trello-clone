304fca3d835d728b80a2e5a263aa656ea78507b4
LeeArtur <arturlee2006@gmail.com>
2021-03-30 18:28:25 +0600

    Finished card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import Card from "./components/Card/CardComponent/Card";
-import BoardForm from "./components/BoardForm/BoardForm";
-import Modal from "./components/UI/Modal/Modal";
 
 const App = () => {
   return (
     <div>
       <Card/>
-      <Modal show={true}>
-          <BoardForm/>
-      </Modal>
     </div>
   );
 };
